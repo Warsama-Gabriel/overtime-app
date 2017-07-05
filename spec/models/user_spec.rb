776e7ade53285e097a8ed8313503543cec73906d
Warsama Gabriel <srobleh@gmail.com>
2017-07-04 17:21:31 -0700

    added user validations, update readme

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #wrap in block to descibr the type of test
+  before do
+    @user = User.create(email: "testmail@mail.com", password: "asdfasdf", password_confirmation: "asdfasdf", firstname: "john", lastname: "snow")
+  end
+  describe "creation" do
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+
+    it "can not be created without firstname, lastname" do
+      @user.firstname = nil
+      @user.lastname = nil
+      expect(@user).to_not be_valid
+    end
+  end
 end
