2446cf84d99fc391d1fd1129a52065b63efdb053
Warsama Gabriel <srobleh@gmail.com>
2017-07-09 19:39:31 -0700

    Added seeds created Posts controller actions, views. Refactor posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,35 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:show]
   def index
   end
 
+  def new
+    # render the new post in the view
+    @post = Post.new
+  end
+
+  def create
+    # save it in the database
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Your post was created successfully'
+    else
+      render :new
+    end
+  end
+
+  def show
+    # find the post from the database
+  end
+
+  private
+
+  def post_params
+    # require bring in the post model and whitelist the post attr coming in
+    params.require(:post).permit(:rationale, :date)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,24 @@ describe 'navigate' do
       expect(page).to have_content('Posts')
     end
   end
+
+  describe 'creation' do
+    before do
+      visit new_post_path
+    end
+    # user vists and can fill out the form to post
+    # the post is associated with the user
+    it 'has a form that can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'allows users to create a new post from the /new page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+
+      click_on 'Save'
+
+      expect(page).to have_content('Some rationale')
+    end
+  end
 end
